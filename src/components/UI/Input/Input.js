408db8943027bd12e648216712f66953853c39ed
jmoon9 <47996673+jmoon9@users.noreply.github.com>
2020-07-22 21:54:30 -0500

    Form validation and submission

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -3,25 +3,36 @@ import classes from './Input.css'
 
 const input = (props) => {
     let inputElement = null;
+    let validationError = null;
+
+    const inputClasses = [classes.InputElement];
+
+    if (props.invalid && props.shouldValidate && props.touched) {
+        inputClasses.push(classes.Invalid);
+    }
+
+    if (props.invalid && props.touched) {
+        validationError = <p>Please enter a valid input</p>;
+    }
 
     switch(props.elementType) {
         case ('input'):
             inputElement = <input 
-                className={classes.InputElement} 
+                className={inputClasses.join(' ')} 
                 {...props.elementConfig} 
                 value={props.value} 
                 onChange={props.changed} />
             break;
         case ('textarea'):
             inputElement = <textarea 
-                className={classes.InputElement} 
+                className={inputClasses.join(' ')} 
                 {...props.elementConfig} 
                 value={props.value} 
                 onChange={props.changed} />
             break;
         case ('select'):
             inputElement = <select 
-                className={classes.InputElement}
+                className={inputClasses.join(' ')}
                 value={props.value} 
                 onChange={props.changed} >
                     {props.elementConfig.options.map(option => (
@@ -31,7 +42,7 @@ const input = (props) => {
             break;
         default:
             inputElement = <input 
-                className={classes.InputElement} 
+                className={inputClasses.join(' ')} 
                 {...props.elementConfig} 
                 value={props.value}  
                 onChange={props.changed} />
@@ -41,6 +52,7 @@ const input = (props) => {
         <div className={classes.Input}>
             <label className={classes.Label}>{props.label}</label>
             {inputElement}
+            {validationError}
         </div>
     );
 };
